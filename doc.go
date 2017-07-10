15da4d75e296121c51466a2c435661f46606a48d
Andras Belicza <iczaaa@gmail.com>
2017-07-10 15:43:46 +0200

    Godoc formatting.

--- doc.go
+++ doc.go
@@ -6,8 +6,11 @@ Intended use case
 kvcache is intended for caching data
 
 - that can be reproduced at any time
+
 - but reproduction is time or resource consuming
+
 - not feasible to store all in memory
+
 - desirable to persist (remains between application restarts)
 
 Features
@@ -15,11 +18,15 @@ Features
 - Very simple interface. Basically just a Get and a Put operation. Does not support
 changing or removing elements, but it supports removing all elements (reset)
 with the Clear method.
+
 - Optimized. Keys are kept in memory for fast lookups.
+
 - Embedded. You init / create your cache from within your app. No external services
 need to run.
+
 - Persistent. Data (key-value pairs) are written to files in a folder given at
 creation time.
+
 - Supports concurrent access.
 
 Notes
@@ -33,6 +40,7 @@ Implementation restrictions
 
 - Length of version and keys must be less than 64 KB (1<<16 - 1),
 exposed as KeySizeLimit.
+
 - Total data size (total size of values) must not exceed 4 GB (1<<32 - 1),
 exposed as DataSizeLimit.
 
